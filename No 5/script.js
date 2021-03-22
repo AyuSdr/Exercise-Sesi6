bd142c310f4cccba049f0843b1c3493ec64a8467
AyuSdr <78190879+AyuSdr@users.noreply.github.com>
2021-03-22 13:00:39 +0700

    Add files via upload

---
+++ No 5/script.js
@@ -0,0 +1,11 @@
+let tanya = true;
+
+while (tanya) {
+    tanya = prompt('Sebutkan kepanjangan dari LA ?');
+
+    if (tanya === 'Los Angeles') {
+        alert('Selamat jawaban kamu benar');
+    } else {
+        tanya;
+    }
+}
